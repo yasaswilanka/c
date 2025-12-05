1855b2d24c699b37c06d8ff92017b0e0388f0c12
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 12:51:30 +0530

    Add files via upload

---
+++ cse1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am yasaswi\n");
+	printf("i am from  katheru\n");
+	printf("my mother name is jaya \n");
+	printf(" my father name is chinna \n");
+return 0;
+}
